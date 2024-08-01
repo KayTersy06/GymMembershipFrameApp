da42c1b0d316a5ffb2f4dfe1c47b494bf1ced13b
KayTersy06 <94805095+KayTersy06@users.noreply.github.com>
2024-08-01 09:13:05 +0200

    Add files via upload

---
+++ GymMembershipFrameApp.java
@@ -0,0 +1,19 @@
+package gymmembershipframeapp;
+
+import za.ac.tut.ui.GymMembershipFrame;
+
+/**
+ *
+ * @author kayte
+ */
+public class GymMembershipFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       new GymMembershipFrame();
+    }
+    
+}
